a4683fc64455cd6b7f8464f419c6427dd17aaf12
LeeMoofon0222 <moofon0222@gmail.com>
2024-01-18 18:05:38 +0800

    map_craft

--- Assets/New Folder/Script/Map_Craft/map_craft.cs
+++ Assets/New Folder/Script/Map_Craft/map_craft.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(CraftingAPI))]
 public class map_craft : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CraftingAPI craftingAPI;
+    Collider[] colliders; 
     void Start()
     {
-        
+        craftingAPI = GetComponent<CraftingAPI>();
     }
 
     // Update is called once per frame
+    
+
     void Update()
     {
-        Collider[] collider = Physics.OverlapBox(gameObject.transform.position, new Vector3(3, 3, 3), Quaternion.identity, LayerMask.GetMask("Enemy"));
-        foreach (var col in collider)
+        if (Input.GetKeyDown(KeyCode.U))
         {
-            Debug.Log(col);
+            colliders = new Collider[0];
+
+            for (int i = 0; i < gameObject.transform.childCount; i++)
+            {
+                Collider[] childColliders = Physics.OverlapSphere(gameObject.transform.GetChild(i).position, 1);
+                colliders = colliders.Concat(childColliders).ToArray();
+            }
+
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                print(colliders[i].gameObject.name);
+            }
         }
     }
+
 }
